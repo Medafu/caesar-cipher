13976f1c2b3be080751d1b0f4f93c5f1fb0f19b4
Medafu <justrayzzin@gmail.com>
2022-07-27 14:28:50 +0300

    Done

--- main.rb
+++ main.rb
@@ -4,18 +4,21 @@ def caesar_cipher(string, shift)
   letters = ('a'..'z').to_a
   str_arr = string.downcase.split("")
   shifted_arr = str_arr.each_with_index.map do |char, idx|
-    if char >= 'a' && char <= 'z'
-      char = letters[letters.index(char) + shift - letters.length]
-      if string[idx] >= 'A' && string[idx] <= 'Z'
-        char.upcase
-      else
-        char
-      end
+    if shift > 0 && char >= 'a' && char <= 'z'
+      shift_char = letters.index(char) + shift
+      shift_char = shift_char - letters.length if shift_char > letters.length
+      char = letters[shift_char]
+      char = char.upcase if string[idx] >= 'A' && string[idx] <= 'Z'
+      char
+    elsif char >= 'a' && char <= 'z'
+      shift_char = letters.index(char) + shift
+      shift_char + letters.length if shift_char < 0
+      char = letters[shift_char]
+      char = char.upcase if string[idx] >= 'A' && string[idx] <= 'Z'
+      char
     else
       char
     end
   end
   shifted_arr.join("") + "\n"
 end
-
-print caesar_cipher("WhaT a string!", -5)
